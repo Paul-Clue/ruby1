34fe9e6895201fc94fe43ad429ca8f15578e31e2
unknown <paulclue20@gmail.com>
2021-01-06 19:33:32 -0500

    bubble sort algo

---
+++ first_ruby_file.rb
@@ -0,0 +1,20 @@
+def bubble_sort(array)
+  temp = 0
+  array.length.times do
+      array.each_with_index do |v,j|
+          if array[j+1]
+              if array[j] > array[j+1]
+                  temp = array[j]
+                  array[j] = array[j+1]
+                  array[j+1] = temp       
+              end
+          end
+      end
+  end
+  return array
+end
+  
+
+
+num_arr = [9,5,1,3,7]
+print bubble_sort(num_arr)
