48cec4bd698aedcf06d877715359a945b6aafb40
unknown <paulclue20@gmail.com>
2021-01-07 01:44:16 -0500

    satisfy rubocop

--- first_ruby_file.rb
+++ first_ruby_file.rb
@@ -2,17 +2,17 @@ def bubble_sort(array)
   temp = 0
   array.length.times do
     array.each_with_index do |v, j|
-      if array[j + 1]
-        next if v > array[j + 1]
-          temp = array[j]
-          array[j] = array[j + 1]
-          array[j + 1] = temp
-        end
-      end
+      break if j + 1 == array.length
+
+      next unless v > array[j + 1]
+
+      temp = array[j]
+      array[j] = array[j + 1]
+      array[j + 1] = temp
     end
   end
   array
 end
 
-num_arr = [9, 5, 1, 3, 7]
+num_arr = [3, 2, 4, 9, 1]
 print bubble_sort(num_arr)
