e89de2886f5e07bedc873f47076c24e4eb18fe98
unknown <paulclue20@gmail.com>
2021-01-07 17:00:21 -0500

    add new method bubble_sort_by

--- first_ruby_file.rb
+++ first_ruby_file.rb
@@ -16,3 +16,22 @@ end
 
 num_arr = [3, 2, 4, 9, 1]
 print bubble_sort(num_arr)
+
+def bubble_sort_by(array)
+  temp = 0
+  array.length.times do
+    array.each_with_index do |v, j|
+      break if j + 1 == array.length
+      next unless yield(v, array[j + 1]) == 1
+
+      temp = array[j]
+      array[j] = array[j + 1]
+      array[j + 1] = temp
+    end
+  end
+  array
+end
+
+num_arr = [3, 2, 4, 9, 1, 15, 2000, 14]
+# print bubble_sort_by(num_arr) {|first, second| first <=> second}
+print bubble_sort_by(num_arr) { |first, second| first <=> second }
