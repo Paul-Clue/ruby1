93c8cd599f39b2da5cba90bc9e653aff6c39c72e
franklin <franklinbenjamin032@gmail.com>
2021-01-06 21:48:44 -0400

    clean the method, linter fix attempt

--- first_ruby_file.rb
+++ first_ruby_file.rb
@@ -1,20 +1,18 @@
 def bubble_sort(array)
   temp = 0
   array.length.times do
-      array.each_with_index do |v,j|
-          if array[j+1]
-              if array[j] > array[j+1]
-                  temp = array[j]
-                  array[j] = array[j+1]
-                  array[j+1] = temp       
-              end
-          end
+    array.each_with_index do |v, j|
+      if array[j + 1]
+        next if v > array[j + 1]
+          temp = array[j]
+          array[j] = array[j + 1]
+          array[j + 1] = temp
+        end
       end
+    end
   end
-  return array
+  array
 end
-  
 
-
-num_arr = [9,5,1,3,7]
+num_arr = [9, 5, 1, 3, 7]
 print bubble_sort(num_arr)
